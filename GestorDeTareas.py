78de0549a7d7a0069970a4cbd0c60154b8fb11b2
JuanIgnacioMarchetto <tu@email.com>
2024-06-12 09:03:21 -0300

    12/6/2024-09:03

---
+++ GestorDeTareas.py
@@ -0,0 +1,32 @@
+
+
+""" 
+Variables y Tipos Simples
+Definir una lista para almacenar las tareas.
+
+Funciones
+Mostrar Menú:
+Crear una función para mostrar el menú de opciones y tomar la elección del usuario.
+
+Agregar Tarea:
+Función para q el  usuario ingrese una tarea nueva con descripción y fecha de vencimiento.
+
+Listar Tareas:
+Función para mostrar todas las tareas, indicando si están completadas o vencidas.
+
+buscar Tarea:
+Función para permitir al usuario buscar una tarea por su descripción.
+
+Eliminar Tarea:
+Función para permitir al usuario eliminar una tarea seleccionándola por su índice.
+
+Marcar Tarea como Completada:
+Función para permitir al usuario marcar una tarea como completada seleccionándola por su índice.
+
+Bucle Principal
+Crear un bucle principal que muestre el menú y ejecute la acción correspondiente según la elección del usuario.
+
+Validación de Datos
+Implementar validaciones para asegurarse de que la entrada del usuario sea correcta (e.g., descripción no vacía, formato de fecha válido).
+
+ """
