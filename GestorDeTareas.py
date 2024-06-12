782eea60f8071113e080a1e64e9e608bc0f951e0
JuanIgnacioMarchetto <tu@email.com>
2024-06-12 09:07:52 -0300

    12/6/2024-09:07

--- GestorDeTareas.py
+++ GestorDeTareas.py
@@ -12,21 +12,23 @@ Agregar Tarea:
 Función para q el  usuario ingrese una tarea nueva con descripción y fecha de vencimiento.
 
 Listar Tareas:
-Función para mostrar todas las tareas, indicando si están completadas o vencidas.
+Función para mostrar todas las tareas,mostrando si están completadas o vencidas.
 
 buscar Tarea:
-Función para permitir al usuario buscar una tarea por su descripción.
+Función para permitir al usuario buscar una tarea por  descripción.
 
 Eliminar Tarea:
-Función para permitir al usuario eliminar una tarea seleccionándola por su índice.
+Función para  eliminar una tarea  .
 
 Marcar Tarea como Completada:
-Función para permitir al usuario marcar una tarea como completada seleccionándola por su índice.
+Función paramarcar una tarea como completada .
 
 Bucle Principal
-Crear un bucle principal que muestre el menú y ejecute la acción correspondiente según la elección del usuario.
+Bucle que muestre el menú y ejecute la acción elegida.
 
 Validación de Datos
-Implementar validaciones para asegurarse de que la entrada del usuario sea correcta (e.g., descripción no vacía, formato de fecha válido).
+Implementar validaciones para asegurarse de que las entradas  sean correctas (descripcion no vacía, formato de fecha).
 
  """
+ 
+ 
